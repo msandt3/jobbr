440675b81a4ac37e5124842f01426234be85d550
Michael Sandt <michael.sandt@gmail.com>
2025-08-28 21:16:05 -0400

    WIP: DUCKDB Source to support email destination

---
+++ sources/duckdb_source.py
@@ -0,0 +1,15 @@
+import dlt
+from duckdb import duckdb
+@dlt.source
+def duckdb_source(schema_name: str, table_name: str):
+    return get_jobs(schema_name, table_name)
+
+@dlt.resource
+def get_jobs(schema_name: str, table_name: str):
+    con = duckdb.connect(database=dlt.secrets.get('destination.duckdb.credentials'), read_only=True)
+    result = con.execute(f"SELECT title, link, company_name, fit_score, reasoning FROM {schema_name}.{table_name} order by fit_score desc limit 5").df().to_dict(orient='records')
+    yield result
+    con.close()
+
+
+    
